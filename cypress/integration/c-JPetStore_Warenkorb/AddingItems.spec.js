fdcdcd45849fd41996ab14ad79810a1478cec692
cemilözen <coezen73@gmail.com>
2022-08-24 11:41:51 +0200

    addingItems Test Cases Added

--- cypress/integration/c-JPetStore_Warenkorb/AddingItems.spec.js
+++ cypress/integration/c-JPetStore_Warenkorb/AddingItems.spec.js
@@ -1,5 +1,6 @@
 import { petstoreURL } from "../../support/authentication.constant"
 var data = require('../../fixtures/sideBar.json')
+
 describe('Sidebar Functionalities Check', () => {
 
   beforeEach("Navigate to Jpetstore.com ", () => {
@@ -8,31 +9,35 @@ describe('Sidebar Functionalities Check', () => {
     cy.pageLogin()
   })
 
-  it('Side Bar Check', () => {
-
-    cy.wait(500)
-    cy.get('#SidebarContent').each((element, index) => {
-      cy.log('element: ', element.text())
-      const text = element.text()
-      expect(text).to.contain(data.SideBarContent[index])
-     
-
-    })
-
-  })
 
   it('Add items from Dog Module to Shopping Cart', () => {
     cy.wait(500)
     cy.get('[src="../images/dogs_icon.gif"]').click()
     cy.url().should('include','/actions/Catalog.action?viewCategory=&categoryId=DOGS')
+    cy.wait(500)
     cy.contains('K9-DL-01').click() 
     cy.wait(500)
+
     cy.contains('EST-9').click()  
     const text = 'Spotless Male Puppy Dalmation'   
     expect(text).to.eq('Spotless Male Puppy Dalmation')
-    // From here => click'K9-DL-01'-> click(EST-9), -> add to chart  -> assert exist /  Shopping Chart:-> click proceed to 'check out' -> 
-    // Fill out the form with fake info -> click continue -> click confirm -> prove the Payment details are exist (Thank you, your order has been submitted)..
-    // -> click 'return to main menu' -> Assert 'Welcome ABC!' exist... finish the test!!
-  })
+    cy.wait(500)
 
+    cy.get('[class="Button"]').click()
+    cy.get('#Cart').should('be.visible')
+    cy.wait(500)
+
+    cy.get('[name="EST-9"]').clear().type('2')
+    cy.get('[name="updateCartQuantities"]').click()
+    cy.get('[name="EST-9"]').should('have.value','2')
+    cy.wait(500)
+
+    cy.get('[href="/actions/Order.action?newOrderForm="]').click()
+    cy.get('[method="post"]').should('be.visible')
+    cy.wait(500)
+
+    cy.Add_Payment_Details()
+    
+  })
+ 
 })
--- cypress/support/JPetStore_commands.js
+++ cypress/support/JPetStore_commands.js
@@ -117,6 +117,8 @@ Cypress.Commands.add('Account_Info_Text', () => {
 
 })
 
+// Adding Items:
+
 Cypress.Commands.add('Account_Info_Adding', () => {
 
     cy.wait(500)
@@ -169,14 +171,117 @@ Cypress.Commands.add('Account_Info_Adding', () => {
 
 })
 
+Cypress.Commands.add('Add_Payment_Details', () => {
 
+    //Handling with Dropdown Buttons:
 
+    cy.get('[name="order.cardType"]').select('American Express')
+    cy.get('[value="American Express"]').should('be.visible')
 
+    // Filling the blanks:
+    
+    cy.contains('Card Number').click()
+    const Text = '1112 2223 3334 4445 5556'
+    cy.get('[name="order.creditCard"]').clear().type(Text)
+    expect(Text).to.eq('1112 2223 3334 4445 5556')
+  
+    cy.contains('First name:').click()
+    const Text1 = 'Queen'
+    cy.get('[name="order.billToFirstName"]').clear().type(Text1)
+    expect(Text1).to.eq('Queen')
+   
+    cy.contains('Last name:').click()
+    const Text2 = 'Elizabeth II'
+    cy.get('[name="order.billToLastName"]').clear().type(Text2)
+    expect(Text2).to.eq('Elizabeth II')
+   
+    cy.contains('Address 1:').click()
+    const Text3 = 'SW1A 1AA Backhingam Palace'
+    cy.get('[name="order.billAddress1"]').clear().type(Text3)
+    expect(Text3).to.eq('SW1A 1AA Backhingam Palace')
+  
+    cy.contains('Address 2:').click()
+    const Text4 = 'Mayfair 01'
+    cy.get('[name="order.billAddress2"]').clear().type(Text4)
+    expect(Text4).to.eq('Mayfair 01')
+  
+    cy.contains('City:').click()
+    const Text5 = 'London'
+    cy.get('[name="order.billCity"]').clear().type(Text5)
+    expect(Text5).to.eq('London')
+    
+    cy.contains('State:').click()
+    cy.get('[name="order.billState"]').clear()
+    cy.get('[name="order.billState"]').should('have.value','')
+  
+    cy.contains('Zip:').click()
+    const Text6 = 'SW1A'
+    cy.get('[name="order.billZip"]').clear().type(Text6)
+    expect(Text6).to.eq('SW1A')
+   
+    cy.contains('Country:').click()
+    const Text8 = 'United Kingdom'
+    cy.get('[name="order.billCountry"]').clear().type(Text8)
+    expect(Text8).to.eq('United Kingdom')
 
+    // Check box:
 
+    cy.wait(500)
+    cy.get('[name="shippingAddressRequired"]').check().should('be.checked')
 
+    cy.wait(1000)
+    cy.get('[name="newOrder"]').click()
+    cy.url().should('include', '/actions/Order.action')
 
+    // Shipping Address Filling:
 
+    cy.contains('First name:').click()
+    const text1 = 'Cemil'
+    cy.get('[name="order.shipToFirstName"]').clear().type(text1)
+    expect(text1).to.eq('Cemil')
+  
+    cy.contains('Last name:').click()
+    const text2 = 'Oezen'
+    cy.get('[name="order.shipToLastName"]').clear().type(text2)
+    expect(text2).to.eq('Oezen')
+    
+    cy.contains('Address 1:').click()
+    const text3 = 'Roesrather Str. 1'
+    cy.get('[name="order.shipAddress1"]').clear().type(text3)
+    expect(text3).to.eq('Roesrather Str. 1')
+   
+    cy.contains('Address 2:').click()
+    cy.get('[name="order.shipAddress2"]').clear()
+    cy.get('[name="order.shipAddress2"]').should('have.value','')
+    
+    cy.contains('City:').click()
+    const text4 = 'Koeln'
+    cy.get('[name="order.shipCity"]').clear().type(text4)
+    expect(text4).to.eq('Koeln')
+    
+    cy.contains('State:').click()
+    const text5 = 'NRW'
+    cy.get('[name="order.shipState"]').clear().type(text5)
+    expect(text5).to.eq('NRW')
+    
+    cy.contains('Zip:').click()
+    const text6 = '11130'
+    cy.get('[name="order.shipZip"]').clear().type(text6)
+    expect(text6).to.eq('11130')
+   
+    cy.contains('Country:').click()
+    const text7 = 'Deutschland'
+    cy.get('[name="order.shipCountry"]').clear().type(text7)
+    expect(text7).to.eq('Deutschland')
 
+    cy.wait(1000)
+    cy.get('[name="newOrder"]').click()
+    cy.url().should('include', '/actions/Order.action')
 
+    cy.wait(1000)
+    cy.get('.Button').contains('Confirm').click()
+    cy.get('h1').should('have.text', 'HTTP Status 500 – Internal Server Error')
 
+  
+    
+})
